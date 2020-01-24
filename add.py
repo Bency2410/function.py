fff6f5a285d539dadac1ab0af8bc77eef2cb51e9
Bency2410 <bencybenny551@gmail.com>
2020-01-24 07:07:42 +0530

    Function programs in python are completed

---
+++ add.py
@@ -0,0 +1,7 @@
+def add(x,y):
+    sum=x+y
+    return sum
+a=int (input("Enter 1st number: "))
+b=int(input("Enter 2nd number: "))
+result=add(a,b)
+print("Sum of ",a,"and" ,b,"=" ,result)
---
+++ even.py
@@ -0,0 +1,7 @@
+def even(x):
+    if(x%2==0):
+        print ("Even Number")
+    else:
+        print("Odd Number ")
+a=int (input("Enter a number: "))
+even(a)
